64e2f9297ca77013f8eeeed7dc9f0f65bc03e7ce
Tejada120 <71035658+Tejada120@users.noreply.github.com>
2022-04-09 13:43:28 +0200

    Update onduty.js

--- onduty.js
+++ onduty.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'onduty',
     permissions: [],
     execute(message){
-        const channel = message.guild.channels.cache.find(c => c.name === '📃┊fichar-staff');
+        const channel = message.guild.channels.cache.find(c => c.name === 'channel-here');
         const author = message.author.tag
         const time = Math.floor(Date.now() / 1000)
 
@@ -11,3 +11,14 @@ module.exports = {
         message.delete()
     }
 }
+
+ [Delete this lines]
+For offduty just change the name of onduty to offduty, and the message content
+
+IMPORTANT 💥
+
+Change channel-here where you want to register the onduty and offduty messages.
+
+[Delete this lines]
+
+
